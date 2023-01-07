97f850e89e4bdd3990bc84f79335eada0a419e9f
LuckyHariharan <lharihar@uwaterloo.ca>
2023-01-07 11:29:41 -0500

    Setup landing page config
    Setup with Ajevan

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-600 ">
+      <h1 className="text-lg text-red">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
