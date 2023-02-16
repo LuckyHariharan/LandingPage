f2a980cc03d631ecc1ca6591c6bdabf8c085f728
LuckyHariharan <lharihar@uwaterloo.ca>
2023-02-16 09:55:31 -0500

    Responsive Design

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -10,9 +10,13 @@ function App() {
     // <div className="grid grid-rows-4 bg-[url(https://images.unsplash.com/photo-1489189642444-0709966aec44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)] ">
     <div className="grid sm:grid-cols-1  text-slate-700  z-50">
       <nav className="px-2 z-50  border-gray-200 bg-gradient-to-r from-sky-900 to-indigo-900">
-        <div className="container  flex flex-wrap items-center justify-between mx-auto">
-          <div className="flex sm:justify-center ">
-            <img src={donating} className="h-24 mr-3 xl" alt="Flowbite Logo" />
+        <div className=" flex flex-wrap justify-center md:justify-between mx-auto">
+          <div className="">
+            <img
+              src={donating}
+              className="h-0 sm:h-24 mr-3 xl"
+              alt="Flowbite Logo"
+            />
           </div>
           <div className="flex justify-center sm: flex-col">
             <span className="text-4xl font-semibold whitespace-nowrap dark:text-white">
@@ -21,13 +25,35 @@ function App() {
             <p className="flex place-content-center text-2xl text-white px-4 whitespace-normal ">
               Front End Software Engineer
             </p>
+
+            <div className="flex place-content-center text-white space-x-8">
+              <a
+                className=" hover:animate-[pulse_1.5s]"
+                href="https://drive.google.com/file/d/1ri6q6CdXIXWJEO63b7Lt0uyG661GfOBM/view?usp=sharing"
+              >
+                {" "}
+                Resume
+              </a>
+              <a
+                className="hover:animate-[pulse_1.5s]"
+                href="https://github.com/LuckyHariharan"
+              >
+                GitHub
+              </a>
+              <a
+                className="hover:animate-[pulse_1.5s]"
+                href="https://github.com/LuckyHariharan"
+              >
+                LinkedIn
+              </a>
+            </div>
           </div>
 
           <div
             className="hidden w-full text-lg md:block md:w-auto"
             id="navbar-multi-level"
           >
-            <ul className="fixed  top-0 right-0 flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0  ">
+            <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0  ">
               <li>
                 <a
                   href="#about"
@@ -71,31 +97,10 @@ function App() {
       ></div>
       <div
         id="about"
-        className=" w-full 1 py-8 mr-12 px-4 text-lg text-white space-y-4  bg-gradient-to-r from-sky-800 to-indigo-900  "
+        className=" w-full h-12 text-lg text-white space-y-4  bg-gradient-to-r from-sky-800 to-indigo-900  "
       >
         {/* <a href="mailto:lharihar@uwaterloo.ca">Email</a> */}
 
-        <div className="flex place-content-center space-x-8">
-          <a
-            className=" hover:animate-[pulse_1.5s]"
-            href="https://drive.google.com/file/d/1ri6q6CdXIXWJEO63b7Lt0uyG661GfOBM/view?usp=sharing"
-          >
-            {" "}
-            Resume
-          </a>
-          <a
-            className="hover:animate-[pulse_1.5s]"
-            href="https://github.com/LuckyHariharan"
-          >
-            GitHub
-          </a>
-          <a
-            className="hover:animate-[pulse_1.5s]"
-            href="https://github.com/LuckyHariharan"
-          >
-            LinkedIn
-          </a>
-        </div>
         {/* // Nav bar to navigate through portfolio pieces */}
       </div>
 
@@ -118,19 +123,22 @@ function App() {
       >
         <h1 className="text-4xl">SKILLS</h1>
       </div>
-      <div className="text-white grid grid-cols-3 text-2xl  justify-items-center bg-gradient-to-r from-sky-500 to-indigo-900">
+      <div className="text-white grid grid-cols-3 text-2xl border-2 justify-items-center bg-gradient-to-r from-sky-500 to-indigo-900">
         <ul>React</ul>
         <ul>TypeScript</ul>
+        <ul>JavaScript</ul>
         <ul>TailwindCSS</ul>
-        <ul>JavaScript ES6</ul>
         <ul>HTML5</ul>
-        <ul>Node.js</ul>
+        <ul>CSS3</ul>
+        <ul>GraphQL</ul>
+        <ul>Next.js</ul>
         <ul>Agile</ul>
         <ul>Python</ul>
-        <ul>R</ul>
-        <ul>GraphQL</ul>
+        <ul>Git</ul>
+        <ul>Jest</ul>
         <ul>Redux</ul>
-        <ul>Jest/Unit Testing</ul>
+        <ul>R</ul>
+        <ul>VBA</ul>
       </div>
 
       <div
@@ -138,16 +146,17 @@ function App() {
         className=" grid py-20 text-white  justify-items-center bg-gradient-to-r from-sky-500 to-indigo-900 "
       >
         <h1 className="text-4xl">PROJECTS</h1>
-        <div className="order-0 -space-y-4">
-          <p className="text-2xl">Coaching Dashboard</p>{" "}
+        <div>
+          <div className="order-0 -space-y-4">
+            <p className="text-2xl">Coaching Dashboard</p>{" "}
+          </div>
+          <p>
+            Developed a coaching dashboard feature at Skillify, to allow coaches
+            to view every students progress at a glance
+          </p>
+          <img alt="" src={coaching}></img>
         </div>
-        <p>
-          Developed a coaching dashboard feature at Skillify, to allow coaches
-          to view every students progress at a glance
-        </p>
-        <img alt="" src={coaching}></img>
       </div>
-
       <div className="text-white grid py-12 justify-contents-center bg-gradient-to-r from-sky-500 to-indigo-900">
         <form action="mailto:lharihar@uwaterloo.ca" method="post">
           CONTACT ME Name: <input type="text" name="Name"></input>
