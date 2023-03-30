62d75c25a68e209bea7c1b0ccdef45d3e98fa4cf
LuckyHariharan <lharihar@uwaterloo.ca>
2023-03-30 16:46:02 -0400

    Update button

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -3,12 +3,12 @@ import hero1 from "./profile.jpg";
 
 const AboutMe = () => {
   return (
-    <div className="grid  grid-cols-1 md:grid-cols-[5fr_3fr] text-black text-xl p-8">
-      <div className="flex flex-col flex-wrap place-content-center">
+    <div className="grid grid-cols-1 md:grid-cols-[5fr_3fr] text-black text-xl px-8 py-4">
+      <div className="flex flex-col flex-wrap place-content-center space-y-8">
         <h1 className="text-4xl flex md:align-center">
           Hello! My name is Lucky, and I am a front end developer!
         </h1>
-        <p className="">
+        <p className="mx-12">
           I am a Canadian citizen, and in my current role at Skillify, a Toronto
           based startup that provides digital media literacy coaching, I have
           built React games, a coaching dashboard and interactive educational
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ProjectWork from "./ProjectWork";
 import Skills from "./Skills";
 function App() {
   return (
-    <div className="bg-[url(https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)] text-slate-700 ">
+    <div className="bg-[url(https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=forma)] text-slate-700 ">
       <div className="">
         <Navbar />
         <About />
