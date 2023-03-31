f9b96dc001c353a3372032710293528a0a1dcbdb
LuckyHariharan <lharihar@uwaterloo.ca>
2023-03-30 23:54:31 -0400

    Draft 1 up

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -3,20 +3,16 @@ import hero1 from "./profile.jpg";
 
 const AboutMe = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[5fr_3fr] text-black text-xl px-8 py-4">
+    <div className="grid grid-cols-1 md:grid-cols-[5fr_3fr] text-black text-xl px-8 ">
       <div className="flex flex-col flex-wrap place-content-center space-y-8">
-        <h1 className="text-4xl flex md:align-center">
-          Hello! My name is Lucky, and I am a front end developer!
-        </h1>
+        <h1 className="text-4xl flex md:align-center">Hello, I'm Lucky!</h1>
         <p className="mx-12">
-          I am a Canadian citizen, and in my current role at Skillify, a Toronto
-          based startup that provides digital media literacy coaching, I have
-          built React games, a coaching dashboard and interactive educational
-          web development content. I graduated from the University of Waterloo
-          in 2021 as an Actuarial Science major and transitioned into software
-          engineering in 2022. I have a keen eye for design, and I enjoy
-          learning new tools and frameworks. I have experience developing with
-          TypeScript, React, Redux, TailwindCSS, Jest, and GraphQL.
+          In my current role as frontend engineer at Skillify, a Toronto based
+          startup that provides digital media literacy coaching I.... I
+          graduated from the University of Waterloo in 2021 as an Actuarial
+          Science major and in 2022 I transitioned from an actuarial analyst
+          role at a life insurer into a frontend software engineer. I am an
+          eager collaborator and thrive in time sensitive settings.
         </p>
       </div>
       <img
