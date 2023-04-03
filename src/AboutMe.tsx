64bbe02db149d3c310ee676b67a57af71fb903c6
LuckyHariharan <lharihar@uwaterloo.ca>
2023-04-03 14:25:00 -0400

    Update AboutMe.tsx

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -6,19 +6,20 @@ const AboutMe = () => {
     <div className="grid grid-cols-1 place-items-center py-4 md:grid-cols-[3fr_5fr] text-black  px-8 ">
       <img
         alt=""
-        className=" md:w-2/3 md:ml-16 ml-0 w-40  border-4 border-gray-400"
+        className=" md:w-2/3  ml-0 w-40  border-4 border-gray-400"
         src={hero1}
       />{" "}
       <div className="flex flex-col place-content-center space-y-8">
-        <p className="m-6 md:mr-24 text-xl md:text-3xl leading-normal md:leading-relaxed">
+        <p className="m-6 md:mr-22 text-xl md:text-3xl leading-normal md:leading-relaxed">
           Lucky is a frontend developer at{" "}
           <a href="https://www.skillify.ca" className="text-blue-600 underline">
             Skillify
           </a>{" "}
           a Toronto based startup that provides digital media literacy coaching.
-          I graduated from the University of Waterloo in 2021 as an Actuarial
-          Science major and in 2022 transitioned actuarial work into frontend
-          development, in addition to learning Kotlin and mobile development.
+          Lucky graduated from the University of Waterloo in 2021 as an
+          Actuarial Science major and in 2022 transitioned actuarial work into
+          frontend development, in addition to learning Kotlin and mobile
+          development.
         </p>
       </div>
     </div>
