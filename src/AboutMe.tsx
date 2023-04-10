83d7469d49f09dcf4be687e9081bd226e4c08e12
LuckyHariharan <lharihar@uwaterloo.ca>
2023-04-09 23:41:32 -0400

    Add favicon, edit copy

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -4,14 +4,13 @@ import hero1 from "./profile.jpg";
 const AboutMe = () => {
   return (
     <div className="grid grid-cols-1 place-items-center py-4 md:grid-cols-[3fr_5fr] text-black  px-8 ">
-      <img
-        alt=""
-        className=" md:w-2/3   w-3/4  border-4 border-gray-400"
-        src={hero1}
-      />{" "}
-      <div className="flex flex-col place-content-center space-y-8 md:space-y-4">
-        {" "}
-        <div className="flex  md:space-x-20 space-x-8  ml-12 place-content-center md:place-content-start md:py-0 px-16 py-8">
+      <div className="space-y-4">
+        <img
+          alt=""
+          className=" md:w-2/3   w-full  border-4 border-gray-400"
+          src={hero1}
+        />{" "}
+        <div className="flex  md:space-x-20 space-x-8  ml-2 place-content-center md:place-content-start md:py-0  ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="52"
@@ -39,7 +38,10 @@ const AboutMe = () => {
             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
           </svg>
         </div>
-        <p className="m-6 md:mr-22 text-xl md:text-3xl leading-normal md:leading-relaxed">
+      </div>
+      <div className="flex flex-col place-content-center space-y-8 md:space-y-4">
+        {" "}
+        <p className="m-6 md:mr-22 text-2xl md:text-3xl leading-normal md:leading-relaxed">
           I'm Lucky, a frontend engineer at{" "}
           <a href="https://www.skillify.ca" className="text-blue-600 underline">
             Skillify
