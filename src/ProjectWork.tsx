0e97a682b1d0f2deb8e20e87e473741367708102
LuckyHariharan <lharihar@uwaterloo.ca>
2023-05-25 23:41:37 -0400

    Update ProjectWork.tsx

--- src/ProjectWork.tsx
+++ src/ProjectWork.tsx
@@ -9,35 +9,41 @@ const ProjectWork: React.FC = () => {
       <h1 className="text-5xl md:text-6xl flex justify-center">Project Work</h1>
 
       <div className="px-4 py-8 space-y-4 lg:px-40 md:px-40 grid grid-cols-1 xl:grid-cols-[1fr_100px_1fr] text-2xl items-center place-content-center ">
-        <img
-          src={coachingDashboardImg}
-          className="border-4"
-          alt="Coaching Dashboard"
-        />
+        <a href="https://www.skillify.ca" className="text-blue-600 underline">
+          <img
+            src={coachingDashboardImg}
+            className="border-4 hover:opacity-75"
+            alt="Coaching Dashboard"
+          />{" "}
+        </a>{" "}
         <div></div>
-
         <p className="">
           Developed a coaching dashboard to allow Skillify coaches to view every
           student's progress at a glance. The tech stack includes TypeScript,
           React, Tailwind CSS, Redux, and Jest for unit testing.
-        </p>
-        <img
-          src={personalityQuizImg}
-          className="border-4"
-          alt="Personality Quiz"
-        />
+        </p>{" "}
+        <a href="https://www.skillify.ca" className="text-blue-600 underline">
+          <img
+            src={personalityQuizImg}
+            className="border-4 hover:opacity-75"
+            alt="Personality Quiz"
+          />
+        </a>{" "}
         <div></div>
-
         <p className="">
           Developed a personality quiz to help users discover their ideal coding
           career, language, etc. The quiz saves user preferences and returns the
           result. The tech stack includes TypeScript, React, Tailwind CSS, and
           Jest for unit testing.
         </p>
-
-        <img src={reactQuizzesImg} className="border-4" alt="React Quizzes" />
+        <a href="https://www.skillify.ca" className="text-blue-600 underline">
+          <img
+            src={reactQuizzesImg}
+            className="border-4 hover:opacity-75"
+            alt="React Quizzes"
+          />{" "}
+        </a>{" "}
         <div></div>
-
         <p className="">
           Scoped and created React games for students learning their
           multiplication tables. The games are designed to be engaging for users
