a9fca7b7d08571a3c617972558cae4aa74bce99f
LuckyHariharan <lharihar@uwaterloo.ca>
2023-05-26 00:32:02 -0400

    Update SkillsComponent.tsx

--- src/SkillsComponent.tsx
+++ src/SkillsComponent.tsx
@@ -1,11 +1,5 @@
-import React from "react";
-import hero1 from "./busy-project-manager-overwhelmed-by-work.png";
-import donating from "./person-donating.png";
 import reactpng from "./REACTPNG.png";
-import coaching from "./coaching.png";
-import AlienPathway from "./ap.png";
 import JavaScriptpng from "./javascriptwebp.webp";
-import tailwindcsspng from "./tailwindcss.png";
 import htmlpng from "./hmlt5png.png";
 import TypeScriptpng from "./typescriptpng.png";
 import css from "./CSSPNG.png";
@@ -13,13 +7,7 @@ import kotlin from "./Kotlin.jpg";
 import graphql from "./graphql.jpg";
 import nextjspng from "./NextJSPNG.png";
 import agilepng from "./agilepng.png";
-import pythonpng from "./pythonpng.png";
 import git from "./git.png";
-import jest from "./JESTWEBP.webp";
-import redux from "./Reduxpng.png";
-import R from "./RPNG.png";
-import VBA from "./vba.png";
-import submit from "./submit.png";
 const SkillsComponent = () => {
   return (
     <div
