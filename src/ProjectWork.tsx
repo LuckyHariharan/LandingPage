8cc38b18c2d51d1bb40782a95a0c101a1aadc7b1
LuckyHariharan <lharihar@uwaterloo.ca>
2023-10-12 14:05:01 -0400

    Update ProjectWork.tsx

--- src/ProjectWork.tsx
+++ src/ProjectWork.tsx
@@ -11,50 +11,56 @@ const ProjectWork: React.FC = () => {
     >
       <h1 className="text-5xl md:text-6xl flex justify-center">Project Work</h1>
 
-      <div className="px-4 py-8 space-y-4 lg:px-40 md:px-40 grid grid-cols-1 xl:grid-cols-[1fr_100px_1fr] text-2xl items-center place-content-center ">
-        <a href="https://www.skillify.ca" className="text-blue-600 underline">
-          <img
-            src={coachingDashboardImg}
-            className="border-4 hover:scale-105 transform transition-transform duration-300"
-            alt="Coaching Dashboard"
-          />
-        </a>
-        <div></div>
-        <p className="">
-          Developed a coaching dashboard to allow Skillify coaches to view every
-          student's progress at a glance. The tech stack includes TypeScript,
-          React, Tailwind CSS, Redux, and Jest for unit testing.
-        </p>
-        <a
-          href="https://skillify.ca/resources/quizzes/careerQuiz"
-          className="text-blue-600 underline"
-        >
-          <img
-            src={personalityQuizImg}
-            className="border-4 hover:scale-105 transform transition-transform duration-300"
-            alt="Personality Quiz"
-          />
-        </a>
-        <div></div>
-        <p className="">
-          Developed a personality quiz to help users discover their ideal coding
-          career, language, etc. The quiz saves user preferences and returns the
-          result. The tech stack includes TypeScript, React, Tailwind CSS, and
-          Jest for unit testing.
-        </p>
-        <a href="https://www.skillify.ca" className="text-blue-600 underline">
-          <img
-            src={reactQuizzesImg}
-            className="border-4 hover:scale-105 transform transition-transform duration-300"
-            alt="React Quizzes"
-          />
-        </a>
-        <div></div>
-        <p className="">
-          Scoped and created React games for students learning their
-          multiplication tables. The games are designed to be engaging for users
-          and the tech stack includes TypeScript, React, and Tailwind CSS.
-        </p>
+      <div className="px-4 py-16 lg:px-40 md:px-40 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 text-2xl">
+        <div className="text-center">
+          <a href="https://www.skillify.ca" className="text-blue-600 underline">
+            <img
+              src={coachingDashboardImg}
+              className="border-4 hover:scale-105 transform transition-transform duration-300"
+              alt="Coaching Dashboard"
+            />
+          </a>
+          <p className="mt-4">
+            Developed a coaching dashboard to allow Skillify coaches to view
+            every student's progress at a glance. The tech stack includes
+            TypeScript, React, Tailwind CSS, Redux, and Jest for unit testing.
+          </p>
+        </div>
+
+        <div className="text-center">
+          <a
+            href="https://skillify.ca/resources/quizzes/careerQuiz"
+            className="text-blue-600 underline"
+          >
+            <img
+              src={personalityQuizImg}
+              className="border-4 hover:scale-105 transform transition-transform duration-300"
+              alt="Personality Quiz"
+            />
+          </a>
+          <p className="mt-4">
+            Developed a personality quiz to help users discover their ideal
+            coding career, language, etc. The quiz saves user preferences and
+            returns the result. The tech stack includes TypeScript, React,
+            Tailwind CSS, and Jest for unit testing.
+          </p>
+        </div>
+
+        <div className="text-center">
+          <a href="https://www.skillify.ca" className="text-blue-600 underline">
+            <img
+              src={reactQuizzesImg}
+              className="border-4 hover:scale-105 transform transition-transform duration-300"
+              alt="React Quizzes"
+            />
+          </a>
+          <p className="mt-4">
+            Scoped and created React games for students learning their
+            multiplication tables. The games are designed to be engaging for
+            users, and the tech stack includes TypeScript, React, and Tailwind
+            CSS.
+          </p>
+        </div>
       </div>
     </div>
   );
